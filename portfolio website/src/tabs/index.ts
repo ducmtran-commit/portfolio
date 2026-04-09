6005946b4d65ac5e86253866e2cac2471ad3c0c9
ductran0718 <duc.m.tran@sjsu.edu>
2026-04-09 14:13:05 -0700

    Delete index.ts

--- portfolio website/src/tabs/index.ts
+++
@@ -1,4 +0,0 @@
-export { tabs, type TabId } from './config'
-export { ArtworksPanel } from './artworks/ArtworksPanel'
-export { AboutPanel } from './about/AboutPanel'
-export { ArtistStatementsPanel } from './artist-statements/ArtistStatementsPanel'
