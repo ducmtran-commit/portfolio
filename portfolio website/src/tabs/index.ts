83b374471ac983abcbdc1abe88cd61b8ead9eb6d
ductran0718 <duc.m.tran@sjsu.edu>
2026-04-09 13:53:32 -0700

    Create index.ts

---
+++ portfolio website/src/tabs/index.ts
@@ -0,0 +1,4 @@
+export { tabs, type TabId } from './config'
+export { ArtworksPanel } from './artworks/ArtworksPanel'
+export { AboutPanel } from './about/AboutPanel'
+export { ArtistStatementsPanel } from './artist-statements/ArtistStatementsPanel'
